fa95c0cd929223a64a8b0fda31a8e2a17ba34cfe
Michu01 <jakubmichalski01@gmail.com>
2024-03-31 21:59:48 +0200

    Fix text overflow

--- src/components/ContactComponent.tsx
+++ src/components/ContactComponent.tsx
@@ -5,10 +5,10 @@ function ContactComponent() {
   return (
     <div id="contact" className="my-3 p-3 bg-half-black rounded">
       <h2 className="my-3">Contact</h2>
-      <div className="vstack gap-3 align-items-center">
-        <h4><IoDocumentTextOutline /> <a target="_blank" rel="noreferrer" href="cv.pdf">CV</a></h4>
-        <h4><FaEnvelope /> Email: <a target="_blank" rel="noreferrer" href="mailto:jakubmichalski01@gmail.com">jakubmichalski01@gmail.com</a></h4>
-        <h4><FaPhone /> Phone: +48697786022</h4>
+      <div className="vstack gap-3 align-items-center text-nowrap text-truncate">
+        <h5><IoDocumentTextOutline /> <a target="_blank" rel="noreferrer" href="cv.pdf">CV</a></h5>
+        <h5><FaEnvelope /> Email: <a target="_blank" rel="noreferrer" href="mailto:jakubmichalski01@gmail.com">jakubmichalski01@gmail.com</a></h5>
+        <h5><FaPhone /> Phone: +48697786022</h5>
       </div>
     </div>
   );
